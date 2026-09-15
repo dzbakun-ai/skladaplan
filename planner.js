9ebd07d3fe2786f07bd3ce03bac8593e53ab30de
dzbakun-ai <dzbakun@gmail.com>
2026-09-15 10:23:24 +0300

    Update planner.js

--- planner.js
+++ planner.js
@@ -1,6 +1,6 @@
 function plannerView() {
-  return `
-    <div class="planner">
+    return `
+      <div class="planner">
       <h1>Планировщик</h1>
       <p>Здесь будет календарь и планирование отгрузок.</p>
     </div>
